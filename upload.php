4be87cbd0fb2825e5b58cc3dd16c5b54ce7d704e
2TComporation <58370054+2TComporation@users.noreply.github.com>
2019-12-26 11:25:38 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,17 +1,29 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-?>
+<?php $directory = "./userfiles"; 
+    $File_name = $_FILES["userfile"]["name"]; 
+    $Type = $_FILES["userfile"]["type"] ; 
+    $Size = ($_FILES["userfile"]["size"] / 1024); 
+    $File_temp_name = $_FILES["userfile"]["tmp_name"]; 
+    if($Size<= 0){
+    
+            die('cant not upload a file ');
+            }
+    
+if (file_exists($directory . " / " . $_FILES["userfile"]["name"])) 
+        {
+        die($_FILES["userfile"]["name"] . " already exists. "); 
+        } 
+    
+    if(is_uploaded_file($_FILES["userfile"]["tmp_name"])){ 
+    
+            if(!move_uploaded_file($File_temp_name,$directory."/".$File_name)){
+                     die('cant not file'.$File_name); 
+                     
+                }
+            } 
+            else 
+            { 
+              die('attack on file'); 
+                    } 
+                    echo $File_name . " is sucessfully uploaded " . " <br/> " ; 
+                    echo "file size : " .$Size. " <br/> "; 
+                    echo"type :".$Type." <br/> "; ?>
